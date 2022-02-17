cab1ebb5144963a26824f4952607de4d592d4c1f
jsrathi17 <jayshreerathi111@gmail.com>
2022-02-18 01:45:07 +0200

    adding codes for mysimbdp

---
+++ code/mysimbdp-coredms.py
@@ -0,0 +1,42 @@
+import datetime
+from cassandra import ConsistencyLevel
+from cassandra.cluster import Cluster
+from cassandra.query import SimpleStatement
+
+
+cluster = Cluster(['0.0.0.0'],port=9042)
+session = cluster.connect()
+#print("i am working")
+
+
+session.execute("""
+            CREATE KEYSPACE IF NOT EXISTS airbnb
+            WITH replication = { 'class': 'SimpleStrategy', 'replication_factor': '2' }
+            """ )
+
+session.set_keyspace("airbnb")
+
+session.execute(""" DROP TABLE IF EXISTS airbnblistings """)
+
+session.execute("""
+            CREATE TABLE IF NOT EXISTS airbnblistings (
+                id int,
+                host_id int,
+                host_name text,
+                neighbourhood text,
+                latitude float,
+                longitude float, 
+                room_type text,
+                price int,
+                minimum_nights int,
+                number_of_reviews int,
+                calculated_host_listings_count int, 
+                availability_365 int,
+                PRIMARY KEY (host_id, id)
+            )
+            """)
+
+print("Created!")
+
+         
+                 
